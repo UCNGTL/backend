840b8fbd754414cacb6fea2c35d86520d85085d5
Marek Vargovcik <vargovcik.marek@gmail.com>
2021-05-22 14:44:23 +0200

    changed github workflow

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import http from 'http';
 import app from './app';
 import config from './utils/config';
 
-const port = config.port;
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(process.env.PORT || config.port);
